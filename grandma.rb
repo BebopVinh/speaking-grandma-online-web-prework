7e3aa3012dd35b95b7629ceb4fd09d106faf8c13
Vinh Do <vinhdo07@gmail.com>
2019-01-14 05:15:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,9 @@ def speak_to_grandma(phrase)
   elsif phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   else
-    puts "HUH?! SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
   end
   binding.pry
 end
+
+  
